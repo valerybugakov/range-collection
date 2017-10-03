016300068a2adc6c0ec0508d1323e9b3da455209
Valery Bugakov <skymk1@gmail.com>
2017-10-03 19:07:50 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
 // @flow
-// Task: Implement a 'Range Collection' class.
-// A pair of integers define a range, for example: [1, 5).
-// This range includes integers: 1, 2, 3, and 4.
-// A range collection is an aggregate of these ranges: [1, 5), [10, 11), [100, 201)
-
-/**
- * RangeCollection class
- * NOTE: Feel free to add any extra member variables/functions you like.
- */
-
 type RangeType = [number, number]
 type RangesType = Array<RangeType>
 
